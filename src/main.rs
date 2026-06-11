70a6c3d459b1b656df43244c2c5eee3667e81d95
JKSoper-ctrl <jamessoper300@gmail.com>
2026-06-11 23:27:56 +0100

    refactor: area function now refactored as a method

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,22 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
+        width: dbg!(15 * scale),
+        height: dbg!(25 * scale),
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
     println!("rect1 is {rect1:?}");
