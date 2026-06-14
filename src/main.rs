a4a60a9762d56fb7579d2dbf8d3cc305c93bb174
JKSoper-ctrl <jamessoper300@gmail.com>
2026-06-14 10:01:53 +0100

    feat: now has 'square' associated function for the Rectangle type

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Self {
+        Self{
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -27,6 +34,7 @@ fn main() {
         width: 60,
         height: 45,
     };
+    let square1 = Rectangle::square(25);
 
     println!("Can rect1 hold rect2 {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3 {}", rect1.can_hold(&rect3));
@@ -38,6 +46,7 @@ fn main() {
 
     println!("rect1 is {rect1:?}");
     dbg!(&rect1);
+    dbg!(&square1);
 }
 
 //this is to test new version of lug
